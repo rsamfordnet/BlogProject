1b9adabb1e72dccdcada78076419dde49cab8b6e
Ryan Samford <rsamfordnet@gmail.com>
2022-11-12 18:12:49 -0500

    adding admin user

---
+++ Services/DataService.cs
@@ -0,0 +1,95 @@
+﻿using BlogProject.Data;
+using BlogProject.Enums;
+using BlogProject.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BlogUser> _userManager;
+
+        public DataService(ApplicationDbContext dbContext, 
+                            RoleManager<IdentityRole> roleManager, 
+                            UserManager<BlogUser> userManager)
+        {
+            _dbContext = dbContext;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task ManageDateAsync()
+        {
+            // Task 1: Seed a few Roles into the system
+            await SeedRolesAsync();
+
+            // Task 2: Seed a few users into the system
+            await SeedUsersAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            //If there are already Roles in the system, do nothing. 
+            if (_dbContext.Roles.Any())
+            { //communication with the ROLES table.
+                return;
+            
+            }
+
+            foreach(var role in Enum.GetNames(typeof(BlogRole)))
+            {
+                // I need to use the Role Manager to create roles
+                await _roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+       private async Task SeedUsersAsync()
+       {
+            //If there are already Users in the system, do nothing.
+            if (_dbContext.Users.Any()) // Checking Users Table for users
+            {
+                return;
+            }
+
+            //Step 1: Creates a new instance of BlogUser
+            var adminUser = new BlogUser()
+            {
+                Email = "casey.spaulding@me.com",
+                UserName = "casey.spaulding@me.com,",
+                FirstName = "Casey",
+                LastName = "Spaulding",
+                EmailConfirmed = true,
+
+            };
+
+            //Step 2: Use the UserManager to create a new user that is defined by adminUser
+
+            await _userManager.CreateAsync(adminUser, "NavyChief1893!!");
+
+            //Step 3; Add this new user to the Administrator role
+
+            await _userManager.AddToRoleAsync(adminUser, BlogRole.Administrator.ToString());
+            //BlogRole is the Enum to assign a role
+
+            //Step 1 repeat: Create the moderator user
+            var modUser = new BlogUser()
+            {
+                Email = "casey.spaulding@me.com",
+                UserName = "casey.spaulding@me.com",
+                FirstName = "Casey",
+                LastName = "Spaulding",
+                EmailConfirmed = true
+            };
+        }
+
+
+
+
+    }
+}
