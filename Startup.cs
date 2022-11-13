d2da30c53f9a5124e4df53bec085926439b538cf
Ryan Samford <rsamfordnet@gmail.com>
2022-11-12 20:25:45 -0500

    Email service class set up

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace BlogProject
 
             //Register a preconfigured instance of the MailSettings class
             services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
-
+            services.AddScoped<IBlogEmailSender, EmailService>();
 
 
         }
